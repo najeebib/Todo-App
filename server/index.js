4c914f6b298511998458dcf433f41196326b8251
najeebib <najeebibraheem1999@hotmail.com>
2024-02-05 14:09:06 +0200

    added api calls to create, fetch, update and delete tasks

--- server/index.js
+++ server/index.js
@@ -1,11 +1,63 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import { fetchTasks, createTask, updateTask, deleteTask } from './task';
+import serverless from "serverless-http";
+import cors from "cors";
+const app = express();
+const port = 3001;
 
+app.use(express.json)
+if(process.env.DEVEOLEMPMENT)
+{
+    app.use(cors());
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/task', async (req, res) => {
+    try {
+        const tasks = await fetchTasks();
+        res.send(tasks.Items)
+    } catch (error) {
+        res.status(400).send(`Error fetching tasks: ${err}`)
+    }
+});
+
+app.post('/task', async(req, res) => {
+    try {
+        const task = req.body;
+        const response = await createTask(task)
+        res.send(response)
+    } catch (error) {
+        res.status(400).send(`Error creating tasks: ${err}`)
+    }
+});
+
+app.put('/task', async(req, res) => {
+    try {
+        const task = req.body;
+        const response = await updateTask(task)
+        res.send(response)
+    } catch (error) {
+        res.status(400).send(`Error updating tasks: ${err}`)
+    }
+});
+
+app.delete('/task/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const response = await deleteTask(id)
+        res.send(response)
+    } catch (error) {
+        res.status(400).send(`Error deleting tasks: ${err}`)
+    }
+});
+if(process.env.DEVEOLEMPMENT)
+{
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    });
+}
+
+
+export const handler = serverless(app)
